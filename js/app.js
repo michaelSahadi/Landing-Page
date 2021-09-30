3f21f15b78a3f638e7398aa19a1ddefb3c06e963
michaelSahadi <michael@michaelsahadi.com>
2021-09-30 01:38:27 +0000

    more junk

--- js/app.js
+++ js/app.js
@@ -1,26 +1,6 @@
-// // When the user scrolls the page, execute myFunction 
-// window.onscroll = function () { myFunction() };
-
-// // Get the header
-// // const header = document.getElementById("navigation");
-// const header = document.getElementById(".nav-positioner");
-
-// // Get the offset position of the navbar
-// const sticky = navigation.offsetTop;
-
-// // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-// function myFunction() {
-//   if (window.pageYOffset > sticky) {
-//     navigation.classList.add("sticky");
-//   } else {
-//     navigation.classList.remove("sticky");
-//   }
-// }
-
 const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 const sections = document.querySelectorAll('section');
-// let sectionsTitle = document.querySelectorAll('section');
 
 hamburger.addEventListener("click", mobileMenu);
 
@@ -28,8 +8,6 @@ function mobileMenu() {
   hamburger.classList.toggle("active");
   navMenu.classList.toggle("active");
 }
-
-
 
 for (let i = 0; i < sections.length; i++) {
   console.log(sections[i].title);
@@ -40,7 +18,6 @@ for (let i = 0; i < sections.length; i++) {
   navMenu.appendChild(listElement);
 }
 
-const active = document.querySelector('li');
 const options = {
   threshold: 0.7
 };
@@ -48,12 +25,17 @@ const options = {
 let observer = new IntersectionObserver(navCheck, options);
 
 function navCheck(entries) {
-  // if (!FileSystemEntry.isIntersecting) {
-  //   console.log(active.innerHTML);
-
   entries.forEach(entry => {
-    console.log(entry);
+    // console.log(entry);
+    const className = entry.target.className;
+    // console.log(className);
+    const activeAnchor = document.querySelector(`[data-page=${className}]`);
+    console.log(activeAnchor);
+    activeAnchor.style.color = "#ffffff";
 
+    if (entry.isIntersecting) {
+      activeAnchor.style.color = "#016774";
+    }
   });
 }
 
